6da6eb001a1e31e7ffdb93638c32f92317115cb0
Patrick McKenzie <Patrick McKenzie>
2023-02-24 17:39:20 -0500

    bug fix

--- simcat/BatchTrain.py
+++ simcat/BatchTrain.py
@@ -436,7 +436,7 @@ class DataGenerator(Sequence):
         # grab rows from sql database
         con = sqlite3.connect(self.sql_path, detect_types=sqlite3.PARSE_DECLTYPES)
         cur = con.cursor()
-        X_ = np.array([self.cur.execute("select arr from counts where id={}".format(_)).fetchone() for _ in list_IDs_temp])
+        X_ = np.array([cur.execute("select arr from counts where id={}".format(_)).fetchone() for _ in list_IDs_temp])
         X_ = X_.reshape(X_.shape[0],X_.shape[2],X_.shape[3])
         con.close()
         ############
